af0f421085f73fa69a6de645b780f8f9a074dd1b
Tiago Behenck dos Santos <tiago.behenck.santos@gmail.com>
2019-06-24 10:23:21 -0300

    First

---
+++ main.js
@@ -0,0 +1,17 @@
+
+class TodoList {
+    constructor() {
+        this.todos = [];
+    }
+
+    addTodo() {
+        this.todos.push('Novo');
+        console.log(this.todos);
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function() {
+    MinhaLista.addTodo();
+}
