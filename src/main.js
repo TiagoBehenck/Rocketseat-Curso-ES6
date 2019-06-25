8ac2c5f78a2f8c5127406632c1e088280fc39c0a
Tiago Behenck dos Santos <tiago.behenck.santos@gmail.com>
2019-06-25 15:20:57 -0300

    Finalizando o curso

--- src/main.js
+++ src/main.js
@@ -1,9 +1,126 @@
-import ClasseUsuario from "./function"
+// import ClasseUsuario from "./function"
+// ClasseUsuario.info();
+// import { Idade } from "./function"
+// console.log(Idade);
+// import Usuario, { idade as IdadeUsuario } from "./functions";
 
-ClasseUsuario.info();
+// const minhaPromise = () => new Promise((resolve, reject) => {
+//     setTimeout(() => { resolve('ok') }, 2000);
+// });
 
-import { Idade } from "./function"
+// async function executaPromise() {
+//     const response = await minhaPromise();
+//     console.log(response);
+// }
 
-console.log(Idade);
+// executaPromise();
 
-import Usuario, { idade as IdadeUsuario } from "./functions";
+// import axios from 'axios';
+
+// class Api {
+//     static async getUserInfo(username) {
+//         try {
+//             const response = await axios.get(`https://api.github.com/users/${username}`);
+//             console.log(response);
+//         } catch (err) {
+//             console.warn('Erro na API!');
+//         }
+//     }
+// }
+
+// Api.getUserInfo('TiagoBehenck');
+
+import api from './api';
+
+class App {
+    constructor() {
+        this.repositories = [];
+
+        this.formEl  = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository]');
+        this.listEl  = document.getElementById('repo-list');
+
+
+        this.registerHandlers();
+    }
+
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    setLoading(loading = true) {
+        if(loading === true) {
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando'));
+            loadingEl.setAttribute('id', 'loading');
+
+            this.formEl.appendChild(loadingEl);
+        } else {
+            document.getElementById('loading').remove();
+        }
+    }
+
+    async addRepository(event) {
+        event.preventDefault();
+
+        const repoInput = this.inputEl.value;
+
+        if (repoInput.length === 0)
+            return;
+
+        this.setLoading();
+        
+        try {
+        const response = await api.get(`/repos/${repoInput}`);
+
+        // Usar desestruturação
+        const { name, description, html_url,  owner: {avatar_url} } = response.data;
+
+        this.repositories.push({
+            name,
+            description,
+            avatar_url,
+            html_url
+        });
+
+        this.inputEl.value = '';
+
+        this.render();
+        } catch (err) {
+            alert('O repo não existe');
+        }
+        
+        this.setLoading(false);
+    }
+
+    render() {
+
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.setAttribute('href', repo.html_url);
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
+    }
+}
+
+new App();
