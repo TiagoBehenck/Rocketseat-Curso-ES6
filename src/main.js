cd8f23d8d6e1bb6e0f17c1398a0058363ce76df6
Tiago Behenck dos Santos <tiago.behenck.santos@gmail.com>
2019-06-25 10:23:12 -0300

    Webpack configurado

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import ClasseUsuario from "./function"
+
+ClasseUsuario.info();
+
+import { Idade } from "./function"
+
+console.log(Idade);
+
+import Usuario, { idade as IdadeUsuario } from "./functions";
